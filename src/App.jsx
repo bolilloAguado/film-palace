651672170a5160da1bf518a106c5ae8e2b9bea9b
bolilloAguado <perrosgatos15.16@gmail.com>
2025-11-30 09:23:39 -0600

    Implemented router

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,19 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Index from "./pages";
 import "./App.css";
-import MainHeader from "./components/MainHeader";
-import Carousel from "./components/Carousel";
-import Subheader from "./components/Subheader";
-import Menu from "./components/Menu";
-import Accordion from "./components/Accordion";
-import MenuItem from "./components/MenuItem";
-import Card from "./components/Card";
-import img1 from "../src/assets/chainsaw-man-the-3840x2160-23013.jpg";
+
 
 function App() {
-  const slides = [
-    img1,
-    "https://media.istockphoto.com/id/1069539210/photo/fantastic-autumn-sunset-of-hintersee-lake.jpg?s=612x612&w=0&k=20&c=oqKJzUgnjNQi-nSJpAxouNli_Xl6nY7KwLBjArXr_GE=",
-    "https://st2.depositphotos.com/1491329/11367/i/450/depositphotos_113672526-Beautiful-nature-scene.jpg",
-  ];
+  return(
+    <Router>
+      {/* <nav>
+        <Link to="/">Index</Link>
+      </nav> */}
 
-  return (
-    <>
-      <div className="flex flex-col max-w-6xl mx-auto text-stone-300 font-amatic-sc grayscale-50 pb-6">
-        <MainHeader />
-        <div className="w-max lg:px-5 px-3 lg:pb-3 pb-2 rounded-md bg-stone-900 lg:ml-10 ml-5 lg:-mb-10 -mb-5 z-20 lg:text-3xl font-dancing-script">
-          Now trending in Film Palace...
-        </div>
-        <Carousel>
-          {slides.map((slide) => (
-            <img src={slide} className="object-cover w-full shrink-0" />
-          ))}
-        </Carousel>
-
-        <Subheader>Popular series</Subheader>
-        <div className="lg:flex gap-8 h-100">
-          <Menu className="w-1/3">
-            <Accordion icon={<i className="bxs bx-film" />} title="Genre">
-              <MenuItem>Action</MenuItem>
-              <MenuItem>Adventure</MenuItem>
-              <MenuItem>Animation</MenuItem>
-              <MenuItem>Anime</MenuItem>
-              <MenuItem>Comedy</MenuItem>
-              <MenuItem>Crime</MenuItem>
-              <MenuItem>Documental</MenuItem>
-              <MenuItem>Drama</MenuItem>
-              <MenuItem>Family</MenuItem>
-              <MenuItem>Fantasy</MenuItem>
-              <MenuItem>Horror</MenuItem>
-            </Accordion>
-
-            <Accordion
-              icon={<i className="bxs bx-calendar-alt-2" />}
-              title="Year"
-            >
-              <MenuItem>2025</MenuItem>
-            </Accordion>
-
-            <Accordion icon={<i className="bxs bx-book" />} title="Type">
-              <MenuItem>Movie</MenuItem>
-            </Accordion>
-          </Menu>
-
-          <div className="pb-5 grow">
-            <div
-              className="flex-1 lg:grid grid-cols-3 gap-5 px-3 lg:my-0 my-5 max-h-100 overflow-hidden overflow-y-scroll
-          [&::-webkit-scrollbar]:w-[3px]
-        [&::-webkit-scrollbar-thumb]:bg-stone-600
-          [&::-webkit-scrollbar-thumb]:rounded-full
-        [&::-webkit-scrollbar-track]:bg-stone-950"
-            >
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ export default function Logo() {
         <div className="text-left font-bold md:text-4xl text-xl -mb-5 font-amatic-sc text-transparent bg-clip-text bg-linear-to-b from-stone-300 to-stone-500">
           Film
         </div>
-        <div className="md:text-5xl text-2xl italic ml-6 font-dancing-script">
+        <div className="md:text-5xl text-2xl italic lg:ml-6 ml-4 font-dancing-script">
           Palace
         </div>
       </div>
